5c3ee36f31b5f01cc5708e08a53443f4a4b3e40a
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-03-16 17:28:46 -0400

    ?=v7

--- public/wk01-noise/OpenSimplex-Noise/sketch.js
+++ public/wk01-noise/OpenSimplex-Noise/sketch.js
@@ -63,7 +63,7 @@ function windowResized() {
 }
 
 function setup_fullScreenBtn() {
-  my.fullScreenBtn = createButton('?=v6 Full Screen');
+  my.fullScreenBtn = createButton('?=v7 Full Screen');
   my.fullScreenBtn.mousePressed(full_screen_action);
   my.fullScreenBtn.style('font-size:42px');
 }
