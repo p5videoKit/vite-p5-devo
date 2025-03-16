a2dfb80b7a9783ab00007984062d06f228e0c74e
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-03-16 15:59:13 -0400

    init

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.js';
+
+id_app.innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite! 2025-01-18 e</h1>
+    <div class="card">
+      <button id="id_counter" type="button"></button>
+      <button id="id_counter2" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(id_counter);
+setupCounter(id_counter2);
